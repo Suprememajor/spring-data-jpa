7dd12fc7fa0d64c117c4d0054d5ecaa221a5518c
Suprememajor <ndipnobertetta@gmail.com>
2021-11-15 11:55:42 +0100

    created student_sequence

--- src/main/java/com/supremecorp/springdatajpa/Student.java
+++ src/main/java/com/supremecorp/springdatajpa/Student.java
@@ -3,7 +3,11 @@ package com.supremecorp.springdatajpa;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 /**
  * Created by suprememajor on the 11/15/21
@@ -16,6 +20,15 @@ import javax.persistence.Id;
 @AllArgsConstructor
 public class Student {
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private Integer age;
     private String email;
